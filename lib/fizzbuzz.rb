abdab621fdc1dd34e3633f44443c221f1fac7d2f
Luke Jones <lukejonesice@yahoo.co.uk>
2019-09-23 16:02:59 +0100

    second passed test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,5 @@
 def fizzbuzz(number)
-  'fizz'
+  (number % 3) == 0 ? num = "Fizz" : num = ""
+  (number % 5) == 0 ? num += "Buzz" : num == "" ? num = number.to_s : nil
+  num
 end
--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -1,7 +1,10 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when past 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when past 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+  it 'returns "Buzz" when past 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
 end
